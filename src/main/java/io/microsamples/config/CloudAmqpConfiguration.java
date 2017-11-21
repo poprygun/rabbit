40e262d7fd04bc43b1639d38fa65f87432020b59
Alex Shumilov <ashumilov@pivotal.io>
2017-11-21 09:30:19 -0500

    Extend configuration from AbstractCloudConfig to simplify service binding

--- src/main/java/io/microsamples/config/CloudAmqpConfiguration.java
+++ src/main/java/io/microsamples/config/CloudAmqpConfiguration.java
@@ -3,9 +3,7 @@ package io.microsamples.config;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.amqp.rabbit.connection.ConnectionFactory;
 import org.springframework.beans.factory.annotation.Value;
-import org.springframework.cloud.Cloud;
-import org.springframework.cloud.CloudFactory;
-import org.springframework.cloud.service.common.AmqpServiceInfo;
+import org.springframework.cloud.config.java.AbstractCloudConfig;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Profile;
@@ -14,20 +12,18 @@ import org.springframework.context.annotation.Profile;
 @Configuration
 @Slf4j
 @Profile("cloud")
-public class CloudAmqpConfiguration {
+public class CloudAmqpConfiguration  extends AbstractCloudConfig {
 
     @Value("${amqp.serviceName:amqp-simple}")
     private String amqpService;
 
-    @Bean
+    @Bean(name = "connectionFactory")
     @Profile("cloud")
-    public ConnectionFactory connectionFactory() {
-        CloudFactory cloudFactory = new CloudFactory();
-        Cloud cloud = cloudFactory.getCloud();
-        AmqpServiceInfo serviceInfo = (AmqpServiceInfo) cloud.getServiceInfo(amqpService);
-        String serviceID = serviceInfo.getId();
-        log.info("Bound to {}", serviceInfo.getId());
-        return cloud.getServiceConnector(serviceID, ConnectionFactory.class, null);
+    public ConnectionFactory amqpConnectionFactory() {
+
+        ConnectionFactory connectionFactory = super.connectionFactory().rabbitConnectionFactory(amqpService);
+        log.info("Bound to {}", amqpService);
+        return connectionFactory;
     }
 
 }
